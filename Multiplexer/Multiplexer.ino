07c121fbbfa14de481833a9eced76d2c21df017b
Tjerk <tjerk@houthuysen.nl>
2018-03-15 08:47:22 +0100

    Improved logging

--- Multiplexer/Multiplexer.ino
+++ Multiplexer/Multiplexer.ino
@@ -1,10 +1,15 @@
 #include <SoftwareSerial.h>
 #include <AltSoftSerial.h>
 
+#include <SPI.h>
+#include <SD.h>
+
 #define dbg Serial    // use Serial for debug
 #define esp Serial1   // use Serial1 for ESP8266
 #define gps Serial2   // use Serial2 for GPS
 #define wnd Serial3   // use Serial3 for AIS
+
+#define DEBUG 1
 
 SoftwareSerial ais(10, 11); // RX, TX
 
@@ -14,6 +19,9 @@ SoftwareSerial ais(10, 11); // RX, TX
 // -----          --------  -------   ------------
 // Arduino Mega      46        48       44, 45
 AltSoftSerial mar; 
+
+const int chipSelect = 4;
+char filename[16];
 
 char aisBuffer[100];
 int aisBufferLength = 0;
@@ -31,7 +39,9 @@ void setup() {
   ais.begin(38400);
   mar.begin(4800);
 
-  dbg.println("Start Multiplexer");
+  writeToLog("Start Multiplexer");
+
+  initSdCard();
 }
 
 void loop() {
@@ -62,8 +72,7 @@ void processWndRead(char c)
   if (c == 0x0A)
   { 
     esp.write(wndBuffer,wndBufferLength);
-    dbg.write(" WND ");
-    dbg.write(wndBuffer,wndBufferLength);
+    writeToLog("WND", wndBuffer, wndBufferLength);
     wndBufferLength = 0;
   }
 }
@@ -77,8 +86,7 @@ void processGpsRead(char c)
   { 
     esp.write(gpsBuffer,gpsBufferLength);
     mar.write(gpsBuffer,gpsBufferLength);
-    dbg.write(" GPS ");
-    dbg.write(gpsBuffer,gpsBufferLength);
+    writeToLog("GPS", gpsBuffer, gpsBufferLength);
     gpsBufferLength = 0;
   }
 }
@@ -91,9 +99,55 @@ void processAisRead(char c)
   if (c == 0x0A)
   { 
     esp.write(aisBuffer,aisBufferLength);
-    dbg.write(" AIS ");
-    dbg.write(aisBuffer,aisBufferLength);
+    writeToLog("AIS", aisBuffer, aisBufferLength);
     aisBufferLength = 0;
+  }
+}
+
+void writeToLog(String logMessage)
+{
+  #if DEBUG == 1
+    dbg.print(millis());
+    dbg.print("\t");
+    dbg.println(logMessage);
+  #endif
+
+  File logFile = SD.open(filename, FILE_WRITE);
+  logFile.print(millis());
+  logFile.print("\t");
+  logFile.println(logMessage);
+  logFile.close();
+}
+
+void writeToLog(char source[3], char logBuffer[100], int logBufferLength)
+{
+  #if DEBUG == 1
+    dbg.print(millis());
+    dbg.print("\t");
+    dbg.write(source);
+    dbg.print("\t");
+    dbg.write(logBuffer,logBufferLength);
+  #endif
+
+  File logFile = SD.open(filename, FILE_WRITE);
+  logFile.print(millis());
+  logFile.print("\t");
+  logFile.write(source);
+  logFile.print("\t");
+  logFile.write(logBuffer,logBufferLength);
+  logFile.close();
+}
+
+void initSdCard() {
+  if (SD.begin(chipSelect)) {
+    int n = 0;
+    snprintf(filename, sizeof(filename), "log%03d.log", n); // includes a three-digit sequence number in the file name
+    while(SD.exists(filename)) {
+      n++;
+      snprintf(filename, sizeof(filename), "log%03d.log", n);
+    }
+  Serial.println(filename);
+  writeToLog(filename);
   }
 }
 
